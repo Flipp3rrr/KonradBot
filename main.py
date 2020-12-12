701e0d549d3cf7a179f12cbc3f330f065acfae3c
Flipp3rrr <flipp3rrr@protonmail.com>
2020-12-12 13:12:02 +0100

    Save bot tokens as "token.txt"

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import discord
 from discord.ext import commands
+import os
+
+runDir = os.path.dirname(__file__)
+try:
+    tokenPath = os.path.join(runDir, "token.txt")
+    with open('token.txt', 'r') as file:
+        btoken = file.read().replace('\n', '')
+        print(btoken)
+except:
+    print("No token.txt found!")
+    btoken = input("TOKEN >> ")
 
 bot = commands.Bot(command_prefix='k! ')
-btoken = input("TOKEN>> ")
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send("Pong!")
+    print("Pinged!")
 
 bot.run(btoken)
