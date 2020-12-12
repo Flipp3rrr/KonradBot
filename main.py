94c11ca11fc4982b7c6c3068220d14d377783f5a
Flipp3rrr <flipp3rrr@protonmail.com>
2020-12-12 13:52:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,28 +12,43 @@ try:
     tokenPath = os.path.join(runDir, "token.txt")
 
     # Read 'token.txt'
-    with open('token.txt', 'r') as file:
-        btoken = file.read().replace('\n', '')
+    with open("token.txt", "r") as file:
+        btoken = file.read().replace("\n", "")
         print("Bot token read from 'token.txt'!")
 except:
     print("No 'token.txt' found!")
     btoken = input("TOKEN >> ")
 
 # Prefix
-bot = commands.Bot(command_prefix='k! ')
+bot = commands.Bot(command_prefix="k! ")
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+    print("USERNAME: ", bot.user.name)
+    print("USER ID:  ", bot.user.id, "\n")
+
+# Member joined
+try:
+    @bot.command()
+    async def joined(ctx, member: discord.Member):
+        await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+except:
+    exception = sys.exc_info()[0]
+    print("Exception: %s" % exception)
 
 # Ping command
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
     print("Pinged!")
+
+# Repeat
+@bot.command()
+async def repeat(ctx, content="repeating..."):
+    filterContent = content.replace("@", "[@]")
+    await ctx.send(filterContent)
+    print("Repeated >", filterContent)
+    print("Original >", content)
 
 print("All set! Running bot...\n")
 bot.run(btoken)
