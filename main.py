ff9d912b78e7d9912d3623cde068266107c0e473
Flipp3rrr <flipp3rrr@protonmail.com>
2020-12-12 13:23:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+# Import modules
+print("Importing 'discord', 'discord.ext', 'os', and 'random'...")
 import discord
 from discord.ext import commands
 import os
+import random
 
+# Get bot token
 runDir = os.path.dirname(__file__)
 try:
+    print("Attempting to read 'token.txt'...")
     tokenPath = os.path.join(runDir, "token.txt")
+
+    # Read 'token.txt'
     with open('token.txt', 'r') as file:
         btoken = file.read().replace('\n', '')
-        print(btoken)
+        print("Bot token read from 'token.txt'!")
 except:
-    print("No token.txt found!")
+    print("No 'token.txt' found!")
     btoken = input("TOKEN >> ")
 
+# Prefix
 bot = commands.Bot(command_prefix='k! ')
 
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+# Ping command
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
     print("Pinged!")
 
+print("All set! Running bot...\n")
 bot.run(btoken)
